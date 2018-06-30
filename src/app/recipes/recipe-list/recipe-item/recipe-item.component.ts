48e143d80e25ab8739f02ce2ef7c27587b7e477d
Amit Kumar Gupta <amitgupatpc@gmail.com>
2018-07-01 00:00:49 +0530

    Module 6 completed

---
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -0,0 +1,23 @@
+///<reference path="../../../../../node_modules/@angular/core/src/metadata/directives.d.ts"/>
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Recipe} from '../../recipe.model';
+
+@Component({
+  selector: 'app-recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.css']
+})
+export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  onSelected() {
+    this.recipeSelected.emit();
+  }
+}
